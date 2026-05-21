ad640a1aa587f00b99272e6113941f55a66c32df
mmdyzn <m.yazdani@gmail.com>
2026-05-21 21:45:51 +0330

    logic: Add win.js for winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,37 @@ import { GameStatus } from "./components/GameStatus"
 import { Header } from "./components/Header"
 import { ScoreBoard } from "./components/ScoreBord"
 import { useState } from "react"
-
+import { WINNING_COMBINATIONS } from "./components/win"
 const INITIAL_GAME_BOARD = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
 function App() {
   const [gameTurns, setGameTurns] = useState([])
-  
-  
+
+
   const gameBoard = [...INITIAL_GAME_BOARD.map(innerItem => [...innerItem])]
   for (let turn of gameTurns) {
-      const { square, symbol } = turn
-      const { rowIndex, colIndex } = square
-      gameBoard[rowIndex][colIndex] = symbol
+    const { square, symbol } = turn
+    const { rowIndex, colIndex } = square
+    gameBoard[rowIndex][colIndex] = symbol
+  }
+  let winner = false
+  const activePlayer = gameTurns.length % 2 == 0 ? "X" : "O"
+
+  function handleSelectSquare(row, col) {
+    if (gameBoard[row][col] !== null) {
+      return;
     }
-  const activePlayer =  gameTurns.length % 2 == 0 ? "X" : "O"
-  
-  function handleSelectSquare(row, col){
+    console.log(gameBoard[row][col])
     setGameTurns(
       prevTurn => {
         const updatedTurn = [
-          ...prevTurn, 
+          ...prevTurn,
           {
-            square:{rowIndex:row, colIndex:col},
+            square: { rowIndex: row, colIndex: col },
             symbol: activePlayer
           }
         ]
@@ -36,15 +41,16 @@ function App() {
       }
     )
   }
-  console.log(gameTurns)
+  // console.log(gameTurns)
+
   return (
     <>
       <Header></Header>
       <div className="w-full max-w-5xl mx-auto mt-10 p-6">
         <div className="grid md:grid-cols-[1fr_2fr_1fr] gap-10">
-        <ScoreBoard ></ScoreBoard>
-        <GameBoard board={gameBoard} onSelect={handleSelectSquare}></GameBoard>
-        <GameStatus></GameStatus>
+          <ScoreBoard ></ScoreBoard>
+          <GameBoard board={gameBoard} onSelect={handleSelectSquare}></GameBoard>
+          <GameStatus></GameStatus>
         </div>
       </div>
     </>
